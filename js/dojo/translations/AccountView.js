be2b0dd4f9b8d964920196f4ac4d861d8764c26b
dvdwalt <dvdwalt@4d75342d-bac5-42b9-a107-79493a2c96c2>
2011-08-08 12:33:30 +0000

    Firts bunch of Danish translations -2
    git-svn-id: svn://svn.code.sf.net/p/hotcakes/code/trunk/yfi@460 4d75342d-bac5-42b9-a107-79493a2c96c2

--- js/dojo/translations/AccountView.js
+++ js/dojo/translations/AccountView.js
@@ -8,37 +8,40 @@ if(!dojo._hasResource["translations.AccountView"]){ //_hasResource checks added 
 
     translations.AccountView.phrases     = {
         'User Detail': {
-			'de_DE' : 'Benutzerdetails',
-	        'es_ES' : 'Detalles de Usuario', 
+            'de_DE' : 'Benutzerdetails',
+            'es_ES' : 'Detalles de Usuario', 
             'af_ZA' : 'Gebruikersinligting',
             'fr_FR' : "Détails de l'utilisateur",
             'ms_MY' : 'Perincian Pengguna',
-		    'nl_NL' : 'Gebruikerdetails',
+            'nl_NL' : 'Gebruikerdetails',
             'id_ID' : 'Detail pengguna',
             'th_TH' : 'รายละเอียดผู้ใช้',
-			'pt_BR' : 'Detalhes do Usuário'
+            'pt_BR' : 'Detalhes do Usuário',
+            'da_DK' : 'Brugerdetaljer'
            },
         'Invoices': {
-			'de_DE' : 'Rechnungen', 
-	        'es_ES' : 'Facturas',
+            'de_DE' : 'Rechnungen', 
+            'es_ES' : 'Facturas',
             'af_ZA' : 'Rekeninge',
             'fr_FR' : 'Factures',
             'ms_MY' : 'Invois',
-		    'nl_NL' : 'Facturen',
+            'nl_NL' : 'Facturen',
             'id_ID' : 'Tagihan',
             'th_TH' : 'ใบกำกับสินค้า/ใบแจ้งหนี้',
-			'pt_BR' : 'Faturas'
+            'pt_BR' : 'Faturas',
+            'da_DK' : 'Opgørelser'
            },
         'Payments': { 
-			'de_DE' : 'Zahlungen',
-	        'es_ES' : 'Pagos',
+            'de_DE' : 'Zahlungen',
+            'es_ES' : 'Pagos',
             'af_ZA' : 'Betalings',
             'fr_FR' : 'Paiements',
             'ms_MY' : 'Bayaran',
-		    'nl_NL' : 'Betalingen',
+            'nl_NL' : 'Betalingen',
             'id_ID' : 'Pembayaran',
             'th_TH' : 'การชำระเงิน',
-			'pt_BR' : 'Pagamentos'
+            'pt_BR' : 'Pagamentos',
+            'da_DK' : 'Betalinger'
            }
     };
 
