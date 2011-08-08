162eb778612243ac6485d982ebc68eff89f5056c
dvdwalt <dvdwalt@4d75342d-bac5-42b9-a107-79493a2c96c2>
2011-08-08 16:05:44 +0000

    Danish translations staring with B
    git-svn-id: svn://svn.code.sf.net/p/hotcakes/code/trunk/yfi@471 4d75342d-bac5-42b9-a107-79493a2c96c2

--- js/dojo/translations/Banner.js
+++ js/dojo/translations/Banner.js
@@ -9,147 +9,160 @@ if(!dojo._hasResource["translations.Banner"]){ //_hasResource checks added by bu
     translations.Banner.phrases     = {
         
         'Change Language': {
-'de_DE' : 'Sprache &auml;ndern',
-	     'es_ES' : 'Cambiar Idioma', 
+            'de_DE' : 'Sprache &auml;ndern',
+            'es_ES' : 'Cambiar Idioma', 
             'af_ZA' : 'Verander Taalvoorkeur',
             'fr_FR' : 'Changer de langue',
-  	     'ms_MY' : 'Tukar Bahasa',
-	     'nl_NL' : 'Wijzig taal',
+            'ms_MY' : 'Tukar Bahasa',
+            'nl_NL' : 'Wijzig taal',
             'id_ID' : 'Ganti Bahasa',
             'th_TH' : 'เปลี่ยนภาษา',
-	     'pt_BR' : 'Alterar Idioma'
+            'pt_BR' : 'Alterar Idioma',
+            'da_DK' : 'Skift sprog'
            },
         'Change Password' : {
-'de_DE' : 'Kennwort aendern',
-	     'es_ES' : 'Cambiar Contraseña',
+            'de_DE' : 'Kennwort aendern',
+            'es_ES' : 'Cambiar Contraseña',
             'af_ZA' : 'Verander Wagwoord',
             'fr_FR' : 'Changer de mot de passe',
-  	     'ms_MY' : 'Tukar Katalaluan',
-	     'nl_NL' : 'Wijzig wachtwoord',
+            'ms_MY' : 'Tukar Katalaluan',
+            'nl_NL' : 'Wijzig wachtwoord',
             'id_ID' : 'Ganti kata sandi',
             'th_TH' : 'เปลี่ยนรหัสผ่าน',
-	     'pt_BR' : 'Alterar Senha'
+            'pt_BR' : 'Alterar Senha',
+            'da_DK' : 'Skift adgangskode'
            },
         'Username' : {
-'de_DE' : 'Benutzername',
-  	     'es_ES' : 'Nombre Usuario',
+            'de_DE' : 'Benutzername',
+            'es_ES' : 'Nombre Usuario',
             'af_ZA' : 'Ingeteken',
             'fr_FR' : "Nom d'utilisateur",
-  	     'ms_MY' : 'Nama Pengguna',
-	     'nl_NL' : 'Gebruikersnaam',
+            'ms_MY' : 'Nama Pengguna',
+            'nl_NL' : 'Gebruikersnaam',
             'id_ID' : 'Username',
             'th_TH' : 'ชื่อผู้ใช้',
-	     'pt_BR' : 'Nome do Usuário'
+            'pt_BR' : 'Nome do Usuário',
+            'da_DK' : 'Brugernavn'
            },
         'Group' : {
-'de_DE' : 'Gruppe',
-	     'es_ES' : 'Grupo',
+            'de_DE' : 'Gruppe',
+            'es_ES' : 'Grupo',
             'af_ZA' : 'Groep',
             'fr_FR' : 'Groupe',
-  	     'ms_MY' : 'Kumpulan',
-	     'nl_NL' : 'Groep',
+            'ms_MY' : 'Kumpulan',
+            'nl_NL' : 'Groep',
             'id_ID' : 'Group',
             'th_TH' : 'กลุ่ม',
-	     'pt_BR' : 'Grupo'
+            'pt_BR' : 'Grupo',
+            'da_DK' : 'Gruppe'
            },
         'Exit' : {
-'de_DE' : 'Exit',
-	     'es_ES' : 'Salir',
+            'de_DE' : 'Exit',
+            'es_ES' : 'Salir',
             'af_ZA' : 'Teken uit',
             'fr_FR' : 'Déconnexion',
-  	     'ms_MY' : 'Keluar',
-	     'nl_NL' : 'Uitloggen',
+            'ms_MY' : 'Keluar',
+            'nl_NL' : 'Uitloggen',
             'id_ID' : 'Keluar',
             'th_TH' : 'ทางออก',
-	     'pt_BR' : 'Sair'
+            'pt_BR' : 'Sair',
+            'da_DK' : 'Afslut'
            },
         'Change Password for' : {
-'de_DE' : 'Kennwort &auml;ndern f&uuml;r',
-	     'es_ES' : 'Cambiar Contraseña de',
+            'de_DE' : 'Kennwort &auml;ndern f&uuml;r',
+            'es_ES' : 'Cambiar Contraseña de',
             'af_ZA' : 'Verander die wagwoord vir',
             'fr_FR' : 'Changer de mot de passe pour',
-  	     'ms_MY' : 'Tukar Katalaluan untuk',
-	     'nl_NL' : 'Wijzig wachtwoord voor',
+            'ms_MY' : 'Tukar Katalaluan untuk',
+            'nl_NL' : 'Wijzig wachtwoord voor',
             'id_ID' : 'Ganti kata sandi untuk',
             'th_TH' : 'เปลี่ยนรหัสผ่านสำหรับ',
-	     'pt_BR' : 'Alterar Senha de'
+            'pt_BR' : 'Alterar Senha de',
+            'da_DK' : 'Skift adgangskode for'
            },
         'New Password' : {
-'de_DE' : 'Neues Kennwort',
-	     'es_ES' : 'Nueva Contraseña',
+            'de_DE' : 'Neues Kennwort',
+            'es_ES' : 'Nueva Contraseña',
             'af_ZA' : 'Nuwe Wagwoord',
             'fr_FR' : 'Nouveau mot de passe',
-  	     'ms_MY' : 'Katalaluan baru',
-	     'nl_NL' : 'Nieuw wachtwoord',
+            'ms_MY' : 'Katalaluan baru',
+            'nl_NL' : 'Nieuw wachtwoord',
             'id_ID' : 'Kata sandi baru',
             'th_TH' : 'รหัสผ่านใหม่',
-	     'pt_BR' : 'Nova Senha'
+            'pt_BR' : 'Nova Senha',
+            'da_DK' : 'Ny adgangskode'
            },
         'Save' : {
-'de_DE' : 'Speichern',
-	     'es_ES' : 'Guardar',
+            'de_DE' : 'Speichern',
+            'es_ES' : 'Guardar',
             'af_ZA' : 'Stoor',
             'fr_FR' : 'Sauver',
-  	     'ms_MY' : 'Simpan',
-	     'nl_NL' : 'Opslaan',
+            'ms_MY' : 'Simpan',
+            'nl_NL' : 'Opslaan',
             'id_ID' : 'Simpan',
             'th_TH' : 'จัดเก็บ',
-	     'pt_BR' : 'Salvar'
+            'pt_BR' : 'Salvar',
+            'da_DK' : 'Gem'
            },
         'Password Changed OK' : {
-'de_DE' : 'Kennwort &auml;ndern OK',
-	     'es_ES' : 'Contraseña Cambiada OK',
+            'de_DE' : 'Kennwort &auml;ndern OK',
+            'es_ES' : 'Contraseña Cambiada OK',
             'af_ZA' : 'Verandering van wagwoord korrek',
             'fr_FR' : 'Mot de passe changé avec succès',
-  	     'ms_MY' : 'Berjaya menukar Katalaluan',
-	     'nl_NL' : 'Wachtwoord succesvol gewijzigd',
+            'ms_MY' : 'Berjaya menukar Katalaluan',
+            'nl_NL' : 'Wachtwoord succesvol gewijzigd',
             'id_ID' : 'Ganti kata sandi berhasil',
             'th_TH' : 'เปลี่ยนรหัสผ่านเรียบร้อยแล้ว',
-	     'pt_BR' : 'Senha Alterada OK'
+            'pt_BR' : 'Senha Alterada OK',
+            'da_DK' : 'Adgangskode ændret!'
            },
         'Change Language for' : {
-'de_DE' : 'Sprache &auml;ndern f&uuml;r',
-	     'es_ES' : 'Cambiar Idioma de',
+            'de_DE' : 'Sprache &auml;ndern f&uuml;r',
+            'es_ES' : 'Cambiar Idioma de',
             'af_ZA' : 'Verander taalvoorkeur vir',
             'fr_FR' : 'Changer de langue pour',
-  	     'ms_MY' : 'Tukar Bahasa untuk',
-	     'nl_NL' : 'Wijzig taal voor',
+            'ms_MY' : 'Tukar Bahasa untuk',
+            'nl_NL' : 'Wijzig taal voor',
             'id_ID' : 'Ganti bahasa untuk',
             'th_TH' : 'เปลี่ยนภาษาสำหรับ',
-	     'pt_BR' : 'Alterar Idioma de'
+            'pt_BR' : 'Alterar Idioma de',
+            'da_DK' : 'Skift sprog for'
            },
         'Language' : {
-'de_DE' : 'Sprache',
-	     'es_ES' : 'Idioma',
+            'de_DE' : 'Sprache',
+            'es_ES' : 'Idioma',
             'af_ZA' : 'Taalvoorkeur',
             'fr_FR' : 'Langue',
-  	     'ms_MY' : 'Bahasa',
-	     'nl_NL' : 'Taal',
+            'ms_MY' : 'Bahasa',
+            'nl_NL' : 'Taal',
             'id_ID' : 'Bahasa',
             'th_TH' : 'ภาษา',
-	     'pt_BR' : 'Idioma'
+            'pt_BR' : 'Idioma',
+            'da_DK' : 'Sprog'
            },
         'Language Changed OK' : {
-'de_DE' : 'Sprache ge&auml;ndert',
-  	     'es_ES' : 'Idioma Cambiado OK',
+            'de_DE' : 'Sprache ge&auml;ndert',
+            'es_ES' : 'Idioma Cambiado OK',
             'af_ZA' : 'Taalvoorkeur het reg verander',
             'fr_FR' : 'Langue changée avec succès',
-  	     'ms_MY' : 'Berjaya menukar Bahasa',
-	     'nl_NL' : 'Taal succesvol gewijzigd',
+            'ms_MY' : 'Berjaya menukar Bahasa',
+            'nl_NL' : 'Taal succesvol gewijzigd',
             'id_ID' : 'Penggantian bahasa berhasil',
             'th_TH' : 'เปลี่ยนภาษาเรียบร้อยแล้ว',
-	     'pt_BR' : 'Idioma Alterado OK'
+            'pt_BR' : 'Idioma Alterado OK',
+            'da_DK' : 'Sprog er ændret!'
            },
         'Log out to activate' : {
-'de_DE' : 'F&uuml;r Aktivierung bitte abmelden',
-	     'es_ES' : 'Cerrar sesión para activar',
+            'de_DE' : 'F&uuml;r Aktivierung bitte abmelden',
+            'es_ES' : 'Cerrar sesión para activar',
             'af_ZA' : 'Teken uit om te aktiveer',
             'fr_FR' : 'Déconnectez-vous pour activer le changement',
-  	     'ms_MY' : 'Log-keluar untuk mengaktifkan',
-	     'nl_NL' : 'Log uit om te activeren',
+            'ms_MY' : 'Log-keluar untuk mengaktifkan',
+            'nl_NL' : 'Log uit om te activeren',
             'id_ID' : 'Log out untuk mengaktifkan',
             'th_TH' : 'ออกจากระบบเพื่อกระตุ้นการทำงาน',
-	     'pt_BR' : 'Fazer o logout para ativar'
+            'pt_BR' : 'Fazer o logout para ativar',
+            'da_DK' : 'Log ud for at aktivere ændringer'
            }
     };
 
