81dfd5c5e4f30261c8344c6b876d94c595295f3a
dvdwalt <dvdwalt@4d75342d-bac5-42b9-a107-79493a2c96c2>
2011-08-13 19:45:02 +0000

    Danish translations up to R
    git-svn-id: svn://svn.code.sf.net/p/hotcakes/code/trunk/yfi@476 4d75342d-bac5-42b9-a107-79493a2c96c2

--- js/dojo/translations/RadiusTest.js
+++ js/dojo/translations/RadiusTest.js
@@ -8,54 +8,59 @@ if(!dojo._hasResource["translations.RadiusTest"]){ //_hasResource checks added b
 
     translations.RadiusTest.phrases     = {
         'Testing RADIUS please wait': {
-'de_DE' : 'RADIUS wird getestet, bitte warten',
-	     'es_ES' : 'Comrprobando RADIUS, porfavor espere', 
+            'de_DE' : 'RADIUS wird getestet, bitte warten',
+            'es_ES' : 'Comrprobando RADIUS, porfavor espere', 
             'af_ZA' : "Toets RADIUS - net 'n oomblik",
             'fr_FR' : 'Test RADIUS en cours - veuillez patienter',
-	     'ms_MY' : 'Sedang menguji RADIUS, sila tunggu',
-	     'nl_NL' : 'Radius testen - een moment',
+            'ms_MY' : 'Sedang menguji RADIUS, sila tunggu',
+            'nl_NL' : 'Radius testen - een moment',
             'id_ID' : 'Mengetes radius',
-	     'pt_BR' : 'Testando o RADIUS, por favor aguarde'
+            'pt_BR' : 'Testando o RADIUS, por favor aguarde',
+            'da_DK' : 'Tester RADIUS - vent venligst...'
         },
         'Send': { 
-'de_DE' : 'Senden',
-	     'es_ES' : 'Enviar',
+            'de_DE' : 'Senden',
+            'es_ES' : 'Enviar',
             'af_ZA' : 'Gestuur',
             'fr_FR' : 'Envoi',
-	     'ms_MY' : 'Hantar',
-	     'nl_NL' : 'Verzonden',
+            'ms_MY' : 'Hantar',
+            'nl_NL' : 'Verzonden',
             'id_ID' : 'Kirim',
-	     'pt_BR' : 'Enviar'
+            'pt_BR' : 'Enviar',
+            'da_DK' : 'Send'
         },
         'Username': { 
-'de_DE' : 'Benutzername',
-	     'es_ES' : 'Nombre Usuario',
+            'de_DE' : 'Benutzername',
+            'es_ES' : 'Nombre Usuario',
             'af_ZA' : 'Gebruikersnaam',
             'fr_FR' : "Nom d'utilisateur",
-	     'ms_MY' : 'Nama Pengguna',
-	     'nl_NL' : 'Gebruikersnaam',
+            'ms_MY' : 'Nama Pengguna',
+            'nl_NL' : 'Gebruikersnaam',
             'id_ID' : 'Username',
-	     'pt_BR' : 'Nome do Usuário'
+            'pt_BR' : 'Nome do Usuário',
+            'da_DK' : 'Brugernavn'
         },
         'Password': { 
-'de_DE' : 'Kennwort',
-	     'es_ES' : 'Contraseña',
+            'de_DE' : 'Kennwort',
+            'es_ES' : 'Contraseña',
             'af_ZA' : 'Wagwoord',
             'fr_FR' : 'Mot de passe',
-	     'ms_MY' : 'Katalaluan',
-	     'nl_NL' : 'Wachtwoord',
+            'ms_MY' : 'Katalaluan',
+            'nl_NL' : 'Wachtwoord',
             'id_ID' : 'Kata sandi',
-	     'pt_BR' : 'Senha'
+            'pt_BR' : 'Senha',
+            'da_DK' : 'Adgangskode'
         },
         'Return':{ 
-'de_DE' : 'Zur&uuml;ck',
-	     'es_ES' : 'Volver',
+            'de_DE' : 'Zur&uuml;ck',
+            'es_ES' : 'Volver',
             'af_ZA' : 'Terugvoer',
             'fr_FR' : 'Réponse',
-	     'ms_MY' : 'Kembali',
-	     'nl_NL' : 'Antwoord',
+            'ms_MY' : 'Kembali',
+            'nl_NL' : 'Antwoord',
             'id_ID' : 'Kembali',
-	     'pt_BR' : 'Retorno'
+            'pt_BR' : 'Retorno',
+            'da_DK' : 'Svar'
         }
     };
 
